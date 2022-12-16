575fdd84c0465f5ad45135a78636611d50f6a4ff
Mikey Shean <mdshean2@gmail.com>
2022-12-15 23:38:59 -0800

    Add admin views for integration and category

--- integrations/admin.py
+++ integrations/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from integrations.models import Integration, Category
+from django.urls import reverse
+from django.utils.safestring import mark_safe 
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+    )
+
+
+@admin.register(Integration)
+class IntegrationAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'integration_link',
+        'category_link'
+    )
+    readonly_fields = ('category_link',)
+
+    def category_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:integrations_category_change", args=(obj.category.pk,)),
+            obj.category.name
+        ))
+    category_link.short_description = 'category'
+
+    def integration_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:integrations_integration_change", args=(obj.pk,)),
+            obj.name
+        ))
+    integration_link.short_description = 'integration'
--- integrations/models.py
+++ integrations/models.py
@@ -5,7 +5,16 @@ from core.models import TimestampedModel
 class Category(TimestampedModel):
     name = models.CharField(max_length=32, help_text="Name of the category")
 
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self) -> str:
+        return self.name
+
 
 class Integration(TimestampedModel):
     name = models.CharField(max_length=64, help_text="Name of the integration")
     category = models.ForeignKey("Category", on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__ (self) -> str:
+        return self.name
