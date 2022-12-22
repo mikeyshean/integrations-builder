9a6a86b0fd297abda649b0da10f18be600cf02fa
Mikey Shean <mdshean2@gmail.com>
2022-12-22 13:21:41 -0800

    Refactor mappers to use new schema (#6)

--- mappers/services/transformer_service.py
+++ mappers/services/transformer_service.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-from mappers.exceptions import NotFoundError
 from mappers.field_transformers import FieldTransformerFactory
 from mappers.models import Transformer
 
@@ -12,11 +11,7 @@ class TransformerService:
     def get_transformer_by_id(self, id: int) -> Transformer:
         return Transformer.objects.filter(id=id).first()
 
-    def transform(self, id: int, value: Any) -> Any:
-        transformer = self.get_transformer_by_id(id)
-        if not transformer:
-            raise NotFoundError("Transformer not found")
-
+    def transform(self, transformer: Transformer, value: Any) -> Any:
         field_transformer = self._transformer_factory.get_transformer_by_type(
             transformer.type
         )
