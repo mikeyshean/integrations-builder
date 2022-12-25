5f769f3d50b5db0dd74c7b03a902e764a8607896
Mikey Shean <mdshean2@gmail.com>
2022-12-25 11:34:36 -0800

    Add JWT token handling (#8)
    * Add JWT token
    
    * add cors origins

---
+++ core/api/views.py
@@ -0,0 +1,26 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["first_name"] = user.first_name
+
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+@api_view(["GET"])
+def get_routes(request):
+    routes = ["/api/token", "/api/token/refresh", "/api/integrations"]
+
+    return Response(routes)
--- core/urls.py
+++ core/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    re_path(r"^api/", include(("core.api", "core"), namespace="api")),
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("api/", include(("core.api.urls", "core"), namespace="api")),
 ]
