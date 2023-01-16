3b82dc05b4cda404994d86bb71a753b1d9dd637c
Mikey Shean <mdshean2@gmail.com>
2023-01-15 20:22:15 -0800

    Return object_model and list_item_type from API

--- mappers/serializers.py
+++ mappers/serializers.py
@@ -3,10 +3,18 @@ from rest_framework import serializers
 from mappers.models import Field, Model
 
 
+class ObjectModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Model
+        fields = ("id", "name")
+
+
 class FieldSerializer(serializers.ModelSerializer):
+    object_model = ObjectModelSerializer(read_only=True)
+
     class Meta:
         model = Field
-        fields = ("id", "name", "type")
+        fields = ("id", "name", "type", "object_model", "list_item_type")
 
 
 class ModelSerializer(serializers.ModelSerializer):
